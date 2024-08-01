a05eb5efad62fe1f7121092ed99bf7d10032a957
kpennington <kpenningtonn@gmail.com>
2024-07-31 23:40:33 -0300

    fix: keymap move up and lsp hover

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,19 +15,19 @@ return {
   },
 
     {
-      "neovim/nvim-lspconfig",
-      config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-       lspconfig.lua_ls.setup({ capabilities = capabilities })
-        lspconfig.clangd.setup({ capabilities = capabilities })
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.clangd.setup({ capabilities = capabilities })
 
-       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      end,
+            vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
     },
 
 }
